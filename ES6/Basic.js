278590563e07abdfc5e63284c49f36430de0562d
Kwanwoong Park <planetes81@gmail.com>
2016-08-01 16:55:22 +0900

    Basic.js

---
+++ ES6/Basic.js
@@ -0,0 +1,14 @@
+var a = 12;
+function myFunction() {
+    console.log(a);
+
+    var b = 13;
+    if(true) {
+        let c = 14;
+        console.log(b);
+
+    }
+    console.log(c);
+}
+
+myFunction();
